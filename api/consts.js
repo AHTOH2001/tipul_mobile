0a8a9b9ebd2f9c8a9d4d9bb4dbcdbb8b2fad68f2
AHTOH <antonlapytko@gmail.com>
2023-06-07 08:50:57 +0300

    Updated back end domain

--- api/consts.js
+++ api/consts.js
@@ -1,2 +1,2 @@
-export const back_end_domain = 'https://5296-37-214-66-88.ngrok-free.app'
+export const back_end_domain = 'https://tipul.pythonanywhere.com'
 // export const back_end_domain = 'http://127.0.0.1:8000'
